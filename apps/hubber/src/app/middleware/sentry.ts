9883d5b847ae98c3a453c02454260c0753bef6e4
Florian Guitton <florian@secretarium.com>
2023-11-16 12:23:18 +0000

    fix(hubber): Make sure Sentry boots later to capture the environment

--- apps/hubber/src/app/middleware/sentry.ts
+++ apps/hubber/src/app/middleware/sentry.ts
@@ -1,40 +1,69 @@
+import { RequestHandler } from 'express-serve-static-core';
 import * as Sentry from '@sentry/node';
 import * as SecretariumInstruments from '@secretarium/instrumentation';
 import { client as prismaClient } from '../../utils/db';
-import { scp as scpClient } from '@klave/providers';
+import { logger, scp as scpClient } from '@klave/providers';
 
-Sentry.init({
-    dsn: process.env.KLAVE_SENTRY_DSN,
-    release: 'klave@0.0.0',
-    environment: process.env.KLAVE_SENTRY_ENV ?? process.env.NODE_ENV ?? 'development',
-    integrations: [
-        // enable HTTP calls tracing
-        new Sentry.Integrations.Http({ tracing: true }),
-        // enable Express.js middleware tracing
-        new Sentry.Integrations.Express(),
-        new Sentry.Integrations.Prisma({
-            client: prismaClient
-        }),
-        new Sentry.Integrations.Mongo(),
-        new SecretariumInstruments.Sentry.ConnectorTracing({
-            connector: scpClient,
-            domains: ['.sta.klave.network']
-        })
-    ],
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
-});
+let sentryRequestMiddlewareReference: RequestHandler;
+let sentryTracingMiddlewareReference: RequestHandler;
+let sentryErrorMiddlewareReference: RequestHandler;
 
-const fork = <Handler>(func: () => Handler) => {
-    if (process.env.NODE_ENV === 'test')
-        return () => {
-            //
-        };
-    return func();
+const initializeSentry = () => {
+    logger.info('Initializing Sentry');
+    Sentry.init({
+        dsn: process.env.KLAVE_SENTRY_DSN,
+        release: 'klave@0.0.0',
+        environment: process.env.KLAVE_SENTRY_ENV ?? process.env.NODE_ENV ?? 'development',
+        integrations: [
+            // enable HTTP calls tracing
+            new Sentry.Integrations.Http({ tracing: true }),
+            // enable Express.js middleware tracing
+            new Sentry.Integrations.Express(),
+            new Sentry.Integrations.Prisma({
+                client: prismaClient
+            }),
+            new Sentry.Integrations.Mongo(),
+            new SecretariumInstruments.Sentry.ConnectorTracing({
+                connector: scpClient,
+                domains: ['.sta.klave.network']
+            })
+        ],
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0
+    });
+
+    const fork = <Handler>(func: () => Handler) => {
+        if (process.env.NODE_ENV === 'test')
+            return () => {
+                //
+            };
+        return func() as RequestHandler;
+    };
+
+    sentryRequestMiddlewareReference = fork(Sentry.Handlers.requestHandler);
+    sentryTracingMiddlewareReference = fork(Sentry.Handlers.tracingHandler);
+    sentryErrorMiddlewareReference = fork(Sentry.Handlers.errorHandler);
 };
 
-export const sentryRequestMiddleware = fork(Sentry.Handlers.requestHandler);
-export const sentryTracingMiddleware = fork(Sentry.Handlers.tracingHandler);
-export const sentryErrorMiddleware = fork(Sentry.Handlers.errorHandler);
+export const sentryRequestMiddleware: RequestHandler = (req, res, next) => {
+
+    if (!sentryRequestMiddlewareReference)
+        initializeSentry();
+    return sentryRequestMiddlewareReference(req, res, next);
+};
+
+export const sentryTracingMiddleware: RequestHandler = (req, res, next) => {
+
+    if (!sentryTracingMiddlewareReference)
+        initializeSentry();
+    return sentryTracingMiddlewareReference(req, res, next);
+};
+
+export const sentryErrorMiddleware: RequestHandler = (req, res, next) => {
+
+    if (!sentryErrorMiddlewareReference)
+        initializeSentry();
+    return sentryErrorMiddlewareReference(req, res, next);
+};
