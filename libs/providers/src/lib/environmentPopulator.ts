d7de718022c003c1120acedbb5227c472921620a
Florian Guitton <florian@secretarium.com>
2023-11-16 12:01:38 +0000

    chore(providers): List out loaded environment variables on boot

--- libs/providers/src/lib/environmentPopulator.ts
+++ libs/providers/src/lib/environmentPopulator.ts
@@ -5,9 +5,11 @@ export const envOps = {
     initialize: async () => {
         logger.info('Initializing environment variables');
         const env = await prisma.environment.findMany();
-        env.forEach(e => {
-            if (e.value?.length ?? 0 > 0)
+        env.sort((a, b) => a.name.localeCompare(b.name)).forEach(e => {
+            if ((e.value?.length ?? 0) > 0) {
                 process.env[e.name] = e.value ?? undefined;
+                logger.info(`  - ${e.name} = ${e.name.toUpperCase().includes('SECRET') ? '***' : (e.value?.length ?? 0) > 42 ? `${e.value?.substring(0, 42)}...` : e.value}`);
+            }
         });
         if (process.env['KLAVE_LOAD_ENV'] === 'true')
             await Promise.all(Object.entries(process.env).map(async ([key, value]) => {
