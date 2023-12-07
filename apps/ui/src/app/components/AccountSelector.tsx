ae8cad40bd307f29d7194eb67fd74486f58e17fc
Florian Guitton <florian@secretarium.com>
2023-12-07 18:23:50 +0000

    fix(ui): Enable Organisation button in org selection

--- apps/ui/src/app/components/AccountSelector.tsx
+++ apps/ui/src/app/components/AccountSelector.tsx
@@ -43,7 +43,14 @@ const AccountSelector: FC<{ className?: string; }> = ({ className }) => {
                                 {other.map(o => <SelectItem key={o.id} value={o.slug} className='px-3 py-2 hover:text-klave-cyan hover:cursor-pointer overflow-hidden'>{o.slug} <span className='text-slate-400 overflow-hidden'>- {o.name}</span></SelectItem>)}
                                 <SelectItem value='~$NEW$~' className='overflow-clip flex flex-grow px-3 py-2 w-full hover:text-klave-cyan hover:cursor-pointer'><span className='flex flex-grow justify-between items-center gap-1 align-middle'><PlusCircledIcon className='inline' /> Create Organisation</span></SelectItem>
                             </Select.Group>
-                        </> : null}
+                        </>
+                        : <>
+                            <Select.Separator className="text-xs text-slate-400 p-1" />
+                            <Select.Group>
+                                <Select.Label className="text-xs text-slate-400 px-3 py-1">Organisations</Select.Label>
+                                <SelectItem value='~$NEW$~' className='overflow-clip flex flex-grow px-3 py-2 w-full hover:text-klave-cyan hover:cursor-pointer'><span className='flex flex-grow justify-between items-center gap-1 align-middle'><PlusCircledIcon className='inline' /> Create Organisation</span></SelectItem>
+                            </Select.Group>
+                        </>}
                 </Select.Viewport>
                 <Select.ScrollDownButton>
                     <ChevronDownIcon />
