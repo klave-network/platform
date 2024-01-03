5daf2834790c5e9947b687827dbc555741823a9b
Florian Guitton <florian@secretarium.com>
2024-01-03 15:43:35 +0000

    chore: Remove deployer lib

--- libs/deployer/README.md
+++
@@ -1,7 +0,0 @@
-# deployer
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test deployer` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/deployer/src/lib/dummyVmFs.ts
+++
@@ -1,9 +0,0 @@
-export const dummyMap: Record<string, string | null> = {
-    'asconfig.json': `
-        {
-            "options": {
-                "bindings": "esm",
-                "disable": "bulk-memory"
-            }
-        }`
-};
