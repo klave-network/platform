1147e0c10972ad3e43f19dccfbca3b1cb470f4be
Florian Guitton <florian@secretarium.com>
2024-02-16 09:39:39 +0000

    chore: Update to tip of Nx 17 branch

--- apps/ui-e2e/playwright.config.ts
+++ apps/ui-e2e/playwright.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
-
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:4220';
-
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
 // require('dotenv').config();
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -28,5 +25,9 @@ export default defineConfig({
         url: 'http://localhost:4220',
         reuseExistingServer: !process.env.CI,
         cwd: workspaceRoot
-    }
+    }, projects: [
+        { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+        { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+        { name: 'webkit', use: { ...devices['Desktop Safari'] } }
+    ]
 });
