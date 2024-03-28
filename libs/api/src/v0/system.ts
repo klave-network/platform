a943277a7e8dad17d2e3e2a60d399f05b2e0c81b
Florian Guitton <florian@secretarium.com>
2024-03-28 17:46:35 +0100

    chore(api): Sanitize the environment dump on client side

--- libs/api/src/v0/system.ts
+++ libs/api/src/v0/system.ts
@@ -16,7 +16,35 @@ export const systemRouter = createTRPCRouter({
         .query(async ({ ctx: { session: { user } } }) => {
             if (user?.globalAdmin !== true)
                 return {};
-            return { ...process.env } as Record<string, string>;
+            const env = { ...process.env } as Record<string, string>;
+            Object.keys(env).forEach((key) => {
+                const normKey = key.toLocaleUpperCase();
+                const normValue = env[key];
+                if (!normKey.startsWith('KLAVE') && !normKey.startsWith('NODE') && !normKey.startsWith('HOSTNAME')) {
+                    delete env[key];
+                    return;
+                }
+                if (!normValue) {
+                    env[key] = '***';
+                    return;
+                }
+                if ((normKey === 'KLAVE_MONGODB_URL' || normKey === 'KLAVE_SMTP_HOST') && normValue) {
+                    try {
+                        const url = new URL(normValue);
+                        env[key] = url.protocol + '//' + url.host + url.pathname;
+                    } catch (e) {
+                        env[key] = '***';
+                    }
+                    return;
+                }
+                if (normKey.includes('SECRET') || normKey.includes('KEY') || normKey.includes('TOKEN')) {
+                    if (['KLAVE_SECRETARIUM_NODE'].includes(normKey))
+                        return;
+                    env[key] = normValue.substring(0, 10) + '***';
+                    return;
+                }
+            });
+            return env;
         }),
     version: publicProcedure
         .query(async () => {
