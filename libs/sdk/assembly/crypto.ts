84fd4eba6cb38d8bbd216032e3271631298d9359
Jeremie Labbe <jeremie@secretarium.org>
2024-06-21 18:06:19 +0100

    feat(sdk): Do not return null when return u8[]

--- libs/sdk/assembly/crypto.ts
+++ libs/sdk/assembly/crypto.ts
@@ -240,12 +240,12 @@ class CryptoECDSA {
 class CryptoSHA {
 
     static isValidAlgorithm(algorithm: string): boolean {
-        if (algorithm != "sha2-256" && algorithm != "sha256" &&
-            algorithm != "sha2-384" && algorithm != "sha384" &&
-            algorithm != "sha2-512" && algorithm != "sha512" &&
-            algorithm != "sha3-256" && 
-            algorithm != "sha3-384" && 
-            algorithm != "sha3-512")
+        if (algorithm != "sha2-256" && algorithm != "sha2_256" && algorithm != "sha256" &&
+            algorithm != "sha2-384" && algorithm != "sha2_384" && algorithm != "sha384" &&
+            algorithm != "sha2-512" && algorithm != "sha2_512" && algorithm != "sha512" &&
+            algorithm != "sha3-256" && algorithm != "sha3_256" && 
+            algorithm != "sha3-384" && algorithm != "sha3_384" && 
+            algorithm != "sha3-512" && algorithm != "sha3_512")
             return false;
         return true;
     }
@@ -254,15 +254,21 @@ class CryptoSHA {
         switch (algorithm)
         {
         case "sha3-256": 
+        case "sha3_256": 
         case "sha2-256": 
+        case "sha2_256": 
         case "sha256": 
             return 32;
         case "sha3-384": 
+        case "sha3_384": 
         case "sha2-384": 
+        case "sha2_384": 
         case "sha384": 
             return 48;
         case "sha3-512": 
+        case "sha3_512": 
         case "sha2-512": 
+        case "sha2_512": 
         case "sha512": 
             return 64;
         default:            
@@ -271,15 +277,17 @@ class CryptoSHA {
         return 0;
     }
 
-    static digest(algorithm: string, data: string): u8[] | null 
+    static digest(algorithm: string, data: string): u8[]
     {        
+        let ret: u8[] = [];
         if (!this.isValidAlgorithm(algorithm))
-            return null;
+            return ret;
 
         return CryptoImpl.digest(algorithm, data);
     }
 
-    static digest_deprecated(data: string): u8[] | null {
+    static digest_deprecated(data: string): u8[] 
+    {
         return this.digest('sha2-256', data);
     }
 }
