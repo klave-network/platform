2a00a67b45d3326c627d686463734edbb5b65bbd
Florian Guitton <florian@secretarium.com>
2024-08-08 17:31:54 +0100

    chore(dispatcher): Update environment variables

--- apps/dispatcher/src/utils/sentry.ts
+++ apps/dispatcher/src/utils/sentry.ts
@@ -8,9 +8,9 @@ export const sentryOps = {
         try {
             console.info('Initializing Sentry');
             Sentry.init({
-                dsn: process.env['KLAVE_SENTRY_DSN'],
+                dsn: process.env['KLAVE_DISPATCH_SENTRY_DSN'],
                 release: `klave@${process.env['GIT_REPO_VERSION']}`,
-                environment: process.env['KLAVE_SENTRY_ENV'] ?? process.env['NODE_ENV'] ?? 'development',
+                environment: process.env['KLAVE_DISPATCH_SENTRY_ENV'] ?? process.env['NODE_ENV'] ?? 'development',
                 integrations: [
                     // enable HTTP calls tracing
                     new Sentry.Integrations.Http({ tracing: true }) as Integration
