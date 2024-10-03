6378df40c1c8e6eae89f4a138d2cdede32e7b848
Florian Guitton <florian@secretarium.com>
2024-10-03 13:37:51 +0100

    chore(hubber): Apply linting fixes

--- apps/hubber/src/app/middleware/probot.ts
+++ apps/hubber/src/app/middleware/probot.ts
@@ -4,7 +4,7 @@ import { probot } from '@klave/providers';
 import probotApp from '../probot';
 
 let middlewareReference: RequestHandler | undefined;
-export const probotMiddleware: RequestHandler = (req, res, next) => {
+export const probotMiddleware: RequestHandler = async (req, res, next) => {
 
     if (!middlewareReference && !(probot as Probot & { uninitialized?: boolean }).uninitialized)
         middlewareReference = createNodeMiddleware(probotApp, {
