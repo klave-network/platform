13cee5c47027e6163db5e0fb3b984369dad85024
Etienne <etienne@secretarium.org>
2024-10-08 17:01:31 +0100

    feat(sdk): Expose Crypto ECDSA

--- libs/sdk/assembly/crypto/index.ts
+++ libs/sdk/assembly/crypto/index.ts
@@ -6,7 +6,7 @@
 import { CryptoImpl, Key } from './crypto_impl';
 import { SubtleCrypto, RsaHashedKeyGenParams as RsaKeyGenInput, RsaOaepParams as RsaOaepInput, AesKeyGenParams as AesKeyGenInput, EcKeyGenParams as EcKeyGenInput } from './crypto_subtle';
 import { CryptoAES } from './crypto_aes';
-// import { CryptoECDSA } from './crypto_ecc';
+import { CryptoECDSA } from './crypto_ecc';
 import { CryptoRSA } from './crypto_rsa';
 import { CryptoSHA } from './crypto_sha';
 import { encode as b64encode } from 'as-base64/assembly';
@@ -17,7 +17,7 @@ export class RsaOaepParams extends RsaOaepInput { }
 export class AesKeyGenParams extends AesKeyGenInput { }
 export class EcKeyGenParams extends EcKeyGenInput { }
 export class AES extends CryptoAES { };
-// export class ECDSA extends CryptoECDSA { };
+export class ECDSA extends CryptoECDSA { };
 export class RSA extends CryptoRSA { };
 export class SHA extends CryptoSHA { };
 
