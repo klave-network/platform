e9a8cb104528dcca809ab3b21a398c840ea7a7b5
Etienne <etienne@secretarium.org>
2024-10-08 11:56:01 +0100

    feat(sdk): Add Export of Simple Crypto lib

--- libs/sdk/assembly/crypto/index.ts
+++ libs/sdk/assembly/crypto/index.ts
@@ -5,10 +5,10 @@
 
 import { CryptoImpl, Key } from './crypto_impl';
 import { SubtleCrypto, RsaHashedKeyGenParams as RsaKeyGenInput, RsaOaepParams as RsaOaepInput, AesKeyGenParams as AesKeyGenInput, EcKeyGenParams as EcKeyGenInput } from './crypto_subtle';
-// import { CryptoAES } from './crypto_aes';
+import { CryptoAES } from './crypto_aes';
 // import { CryptoECDSA } from './crypto_ecc';
-// import { CryptoRSA } from './crypto_rsa';
-// import { CryptoSHA } from './crypto_sha';
+import { CryptoRSA } from './crypto_rsa';
+import { CryptoSHA } from './crypto_sha';
 import { encode as b64encode } from 'as-base64/assembly';
 
 export class Subtle extends SubtleCrypto { }
@@ -16,10 +16,10 @@ export class RsaHashedKeyGenParams extends RsaKeyGenInput { }
 export class RsaOaepParams extends RsaOaepInput { }
 export class AesKeyGenParams extends AesKeyGenInput { }
 export class EcKeyGenParams extends EcKeyGenInput { }
-// export class AES extends CryptoAES { };
+export class AES extends CryptoAES { };
 // export class ECDSA extends CryptoECDSA { };
-// export class RSA extends CryptoRSA { };
-// export class SHA extends CryptoSHA { };
+export class RSA extends CryptoRSA { };
+export class SHA extends CryptoSHA { };
 
 export function getKey(keyName: string): Key | null {    
     if (CryptoImpl.keyExists(keyName))
