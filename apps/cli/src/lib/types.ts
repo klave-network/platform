cfba3cb8ba976106ab9f184ddb296ced2219bf8b
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-21 15:12:38 +0100

    chore(cli): Add helper files

---
+++ apps/cli/src/lib/types.ts
@@ -0,0 +1,47 @@
+import { PromptObject } from 'prompts';
+
+/**
+ * Possible command options.
+ */
+export type CommandOptions = {
+    target: string;
+    source?: string;
+    withReadme: boolean;
+    withChangelog: boolean;
+    example: boolean;
+};
+
+/**
+ * Represents an object that is passed to `ejs` when rendering the template.
+ */
+export type SubstitutionData = {
+    project: {
+        slug: string;
+        version: string;
+        description: string;
+    };
+    author: string;
+    license: string;
+    repo: string;
+};
+
+export type KlaveJson = {
+    version: string;
+    branch: string;
+    applications: HonestApplication[];
+};
+
+export type HonestApplication = {
+    name: string;
+    slug: string;
+    version: string;
+    rootDir: string;
+    id?: string;
+};
+
+export type CustomPromptObject = PromptObject & {
+    name: string;
+    resolvedValue?: string | null;
+};
+
+export type Answers = Record<string, string>;
