f92f39c87fd0f3c5da8692d4548ae746cf4e5fbc
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-22 12:24:37 +0100

    chore(create): Silence 'punycode' error when using yarn

--- apps/create/src/index.ts
+++ apps/create/src/index.ts
@@ -1,3 +1,22 @@
+{
+    const SILENCED_ERRORS = [
+        'DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.'
+    ];
+
+
+    const originalError = console.error;
+
+    console.error = (msg: unknown) => {
+        const isSilencedError = SILENCED_ERRORS.some(
+            error => typeof msg === 'string' && msg.includes(error)
+        );
+        if (isSilencedError) {
+            return;
+        }
+        originalError(msg);
+    };
+}
+
 import spawnAsync from '@expo/spawn-async';
 import chalk from 'chalk';
 import { Command } from 'commander';
