3820a4a5e7ef2439851a79a910ea8716f4904e81
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-22 12:22:25 +0100

    chore(cli): Remove unused files

--- apps/cli/src/lib/utils.ts
+++ apps/cli/src/lib/utils.ts
@@ -1,32 +1,6 @@
-import chalk from 'chalk';
 import spawn from 'cross-spawn';
 import githubUsername from 'github-username';
-import ora, { Options, Ora } from 'ora';
 import { exec } from 'child_process';
-
-export type StepOptions = Options;
-
-export async function newStep<Result>(
-    title: string,
-    action: (step: Ora) => Promise<Result> | Result,
-    options: StepOptions = {}
-): Promise<Result> {
-    const disabled = process.env.CI || process.env.EXPO_DEBUG;
-    const step = ora({
-        text: chalk.bold(title),
-        isEnabled: !disabled,
-        stream: disabled ? process.stdout : process.stderr,
-        ...options
-    });
-    step.start();
-    try {
-        return await action(step);
-    } catch (error) {
-        step.fail();
-        console.error(error);
-        process.exit(1);
-    }
-}
 
 /**
  * Finds user's name by reading it from the git config.
