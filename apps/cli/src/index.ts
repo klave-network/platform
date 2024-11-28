cffa1d4063b21ce5741e2f89038d84ad512a15bc
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-28 12:53:37 +0100

    feat(cli): Add `info` command to output environment info

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -2,8 +2,7 @@ import { Command } from 'commander';
 import chalk from 'chalk';
 import { KLAVE_CYAN_BG } from '~/lib/constants';
 import packageJson from '../package.json';
-import { about } from '~/commands/about';
-import { create } from '~/commands/create';
+import { info } from '~/commands/info';
 
 console.log(KLAVE_CYAN_BG(chalk.bold(` ${packageJson.name} `)), `v${packageJson.version}`, 'The honest-by-design platform.');
 console.log();
@@ -17,7 +16,6 @@ program
     .arguments('[path]');
 
 program
-    .addCommand(about())
-    .addCommand(create());
+    .addCommand(info());
 
 program.parse(process.argv);
