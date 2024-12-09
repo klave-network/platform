7dd39c0a94a346c34e181f063036d3058ac147fd
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-12-09 12:16:12 +0100

    chore(create): Format outro message

--- apps/create/src/index.ts
+++ apps/create/src/index.ts
@@ -185,7 +185,7 @@ Build your application:
 
     - Enter your project directory using ${KLAVE_LIGHT_BLUE(chalk.bold(`cd ${dir}`))}
     ${installDeps ? 'EMPTY_LINE' : `- To install dependencies, run ${KLAVE_LIGHT_BLUE(chalk.bold(`${packageManager} install ${installLegacyDeps}`))}`}
-- To build your application, run ${KLAVE_LIGHT_BLUE(chalk.bold(`${packageManager} ${buildCmd}`))}
+    - To build your application, run ${KLAVE_LIGHT_BLUE(chalk.bold(`${packageManager} ${buildCmd}`))}
     - Log in to ${KLAVE_LIGHT_BLUE(chalk.bold(createTerminalLink('Klave', KLAVE_PLATFORM_URL)))} to deploy your application
 
 Documentation
