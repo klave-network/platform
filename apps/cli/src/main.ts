25b9eacf8c5a450a8ccae6aa90635764d003dc1e
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-01-24 14:40:01 +0100

    fix(cli): Use correct arg when destructuring options

--- apps/cli/src/main.ts
+++ apps/cli/src/main.ts
@@ -41,7 +41,7 @@ export const runCli = async () => {
         .addOption(new Option('-n, --name <name>', 'name of the app'))
         .option('--noInstall', 'skip installing dependencies', false)
         .option('--noGit', 'skip initializing a new git repo in the project', false)
-        .action(async (_name: string, options: CreateOptions) => {
+        .action(async (options: CreateOptions) => {
             await create({
                 name: options.name,
                 template: options.template,
