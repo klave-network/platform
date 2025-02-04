490143e11380e12be9a50b35afe8a1a2eab5260c
Jeremie Labbe <jeremie@secretarium.org>
2025-02-04 11:45:35 +0000

    chore(crate): Add cancel_transaction and replace &str by String in http request and response

--- crates/klave/src/http.rs
+++ crates/klave/src/http.rs
@@ -3,7 +3,7 @@ use http::{Request, Response};
 use super::https::{self, Protocol};
 
 /// Send a http request
-pub fn request(request: &Request<&str>) -> Result<Response<String>, Box<dyn std::error::Error>>
+pub fn request(request: &Request<String>) -> Result<Response<String>, Box<dyn std::error::Error>>
 {
     return https::request_impl(request, Some(Protocol::Http));
 }
