218c824d534e3737ea56d74982f1020cdcaef165
Jeremie Labbe <jeremie@secretarium.org>
2025-02-06 03:22:55 +0000

    feat(crate): Remove http feature

--- crates/klave/src/http.rs
+++
@@ -1,9 +0,0 @@
-
-use http::{Request, Response};
-use super::https::{self, Protocol};
-
-/// Send a http request
-pub fn request(request: &Request<String>) -> Result<Response<String>, Box<dyn std::error::Error>>
-{
-    return https::request_impl(request, Some(Protocol::Http));
-}
