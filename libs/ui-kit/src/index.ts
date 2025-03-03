76bf716b1fc79ffec6506513704f0f801def30cd
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-03-03 12:39:33 +0100

    chore(ui-kit,ui): Combine util and ui-kit library, update path aliases

--- libs/ui-kit/src/index.ts
+++ libs/ui-kit/src/index.ts
@@ -1,3 +1,7 @@
-export { OrganisationConfirmationEmail } from './email-templates/organisation-confirmation';
-export { RegistrationFollowUpEmail } from './email-templates/registration-follow-up';
-export { VerificationCodeEmail } from './email-templates/verification-code';
+// email templates
+export { OrganisationConfirmationEmail } from './components/email-templates/organisation-confirmation';
+export { RegistrationFollowUpEmail } from './components/email-templates/registration-follow-up';
+export { VerificationCodeEmail } from './components/email-templates/verification-code';
+
+// ui components
+export { Button } from './components/ui/button';
