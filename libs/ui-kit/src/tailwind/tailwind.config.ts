7822091aa5abff8778ddc1a325c94f214e1ad6d7
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-03-04 16:49:34 +0100

    chore(ui,ui-kit): Move brand colors to shared tw config

--- libs/ui-kit/src/tailwind/tailwind.config.ts
+++ libs/ui-kit/src/tailwind/tailwind.config.ts
@@ -15,40 +15,51 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                border: 'hsl(var(--border))',
-                input: 'hsl(var(--input))',
-                ring: 'hsl(var(--ring))',
-                background: 'hsl(var(--background))',
-                foreground: 'hsl(var(--foreground))',
-                primary: {
+                'klave-dark-blue': '#00021A',
+                'klave-light-blue': '#00BFFF',
+                'klave-cyan': '#00FFD5',
+                'klave-off-black': '#0E1218',
+                'klave-dark-grey': '#13161D',
+                'klave-mid-grey': '#2F363E',
+                'klave-light-grey': '#808080',
+                'klave-off-white': '#E5E5E5',
+                'klave-red': '#D97873',
+                'klave-peach': '#FCA870',
+                'klave-purple': '#B291ED',
+                'border': 'hsl(var(--border))',
+                'input': 'hsl(var(--input))',
+                'ring': 'hsl(var(--ring))',
+                'background': 'hsl(var(--background))',
+                'foreground': 'hsl(var(--foreground))',
+                'primary': {
                     DEFAULT: 'hsl(var(--primary))',
                     foreground: 'hsl(var(--primary-foreground))'
                 },
-                secondary: {
+                'secondary': {
                     DEFAULT: 'hsl(var(--secondary))',
                     foreground: 'hsl(var(--secondary-foreground))'
                 },
-                destructive: {
+                'destructive': {
                     DEFAULT: 'hsl(var(--destructive))',
                     foreground: 'hsl(var(--destructive-foreground))'
                 },
-                muted: {
+                'muted': {
                     DEFAULT: 'hsl(var(--muted))',
                     foreground: 'hsl(var(--muted-foreground))'
                 },
-                accent: {
+                'accent': {
                     DEFAULT: 'hsl(var(--accent))',
                     foreground: 'hsl(var(--accent-foreground))'
                 },
-                popover: {
+                'popover': {
                     DEFAULT: 'hsl(var(--popover))',
                     foreground: 'hsl(var(--popover-foreground))'
                 },
-                card: {
+                'card': {
                     DEFAULT: 'hsl(var(--card))',
                     foreground: 'hsl(var(--card-foreground))'
                 },
-                sidebar: {
+                'sidebar': {
                     'DEFAULT': 'hsl(var(--sidebar-background))',
                     'foreground': 'hsl(var(--sidebar-foreground))',
                     'primary': 'hsl(var(--sidebar-primary))',
