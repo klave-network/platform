bf9146d1dfb4ba13e6294e126b675042177fdbfb
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-03-05 15:47:07 +0100

    feat(ui): Revamp `AccountSelector` component

--- apps/ui/src/app/components/AccountSelector.tsx
+++ apps/ui/src/app/components/AccountSelector.tsx
@@ -1,10 +1,18 @@
-import { FC, forwardRef, PropsWithChildren, useMemo } from 'react';
-import * as Select from '@radix-ui/react-select';
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon, PlusCircledIcon } from '@radix-ui/react-icons';
+import { FC, useMemo } from 'react';
+import { PlusCircledIcon } from '@radix-ui/react-icons';
 import api from '../utils/api';
 import { useNavigate, useParams } from 'react-router-dom';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue
+} from '@klave/ui-kit/components/ui/select';
 
-const AccountSelector: FC<{ className?: string; }> = ({ className }) => {
+const AccountSelector: FC = () => {
 
     const navigate = useNavigate();
     const { orgSlug } = useParams();
@@ -19,60 +27,39 @@ const AccountSelector: FC<{ className?: string; }> = ({ className }) => {
             navigate(`/${value}`);
     };
 
-    return <Select.Root value={orgSlug} defaultValue={orgSlug} onValueChange={changeValue}>
-        <Select.Trigger className={`select select-bordered select-sm inline-flex justify-between flex-grow w-full items-center text-klave-light-blue bg-white data-[placeholder]:text-klave-light-blue mt-3 mb-5 ${className}`}>
-            <Select.Value placeholder="Select an account" />
-        </Select.Trigger>
-        <Select.Portal>
-            <Select.Content className="overflow-hidden bg-white shadow-outline shadow rounded-lg w-full z-[1000]">
-                <Select.ScrollUpButton>
-                    <ChevronUpIcon />
-                </Select.ScrollUpButton>
-                <Select.Viewport>
-                    {personals.length > 0
-                        ? <Select.Group>
-                            <Select.Label className="text-xs text-slate-400 px-3 py-1">Personal Account</Select.Label>
-                            {personals.map(o => <SelectItem key={o.id} value={o.slug} className="px-3 py-2 hover:text-klave-cyan hover:cursor-pointer">{o.slug.replace('~$~', '')}</SelectItem>)}
-                        </Select.Group>
-                        : null}
-                    {other.length > 0
-                        ? <>
-                            <Select.Separator className="text-xs text-slate-400 p-1" />
-                            <Select.Group>
-                                <Select.Label className="text-xs text-slate-400 px-3 py-1">Organisations</Select.Label>
-                                {other.map(o => <SelectItem key={o.id} value={o.slug} className='px-3 py-2 hover:text-klave-cyan hover:cursor-pointer overflow-hidden'>{o.slug.replace('~$~', '')}</SelectItem>)}
-                                <SelectItem value='~$NEW$~' className='overflow-clip flex flex-grow px-3 py-2 w-full hover:text-klave-cyan hover:cursor-pointer'><span className='flex flex-grow justify-between items-center gap-1 align-middle'><PlusCircledIcon className='inline' /> Create Organisation</span></SelectItem>
-                            </Select.Group>
-                        </>
-                        : <>
-                            <Select.Separator className="text-xs text-slate-400 p-1" />
-                            <Select.Group>
-                                <Select.Label className="text-xs text-slate-400 px-3 py-1">Organisations</Select.Label>
-                                <SelectItem value='~$NEW$~' className='overflow-clip flex flex-grow px-3 py-2 w-full hover:text-klave-cyan hover:cursor-pointer'><span className='flex flex-grow justify-between items-center gap-1 align-middle'><PlusCircledIcon className='inline' /> Create Organisation</span></SelectItem>
-                            </Select.Group>
-                        </>}
-                </Select.Viewport>
-                <Select.ScrollDownButton>
-                    <ChevronDownIcon />
-                </Select.ScrollDownButton>
-            </Select.Content>
-        </Select.Portal>
-    </Select.Root>;
-};
-
-const SelectItem = forwardRef<HTMLDivElement, PropsWithChildren<{
-    value: string;
-    disabled?: boolean;
-    className?: string;
-}>>(({ children, className, ...props }, forwardedRef) => {
     return (
-        <Select.Item className={`flex h-9 items-center select-none relative px-5 data-[disabled]:text-slate-300 data-[disabled]:pointer-events-none data-[highlighted]:text-klave-light-blue data-[highlighted]:bg-blue-100 data-[highlighted]:outline-none ${className}`} {...props} ref={forwardedRef}>
-            <Select.ItemText>{children}</Select.ItemText>
-            <Select.ItemIndicator className="SelectItemIndicator">
-                <CheckIcon />
-            </Select.ItemIndicator>
-        </Select.Item>
+        <Select value={orgSlug} defaultValue={orgSlug} onValueChange={changeValue}>
+            <SelectTrigger className="mb-2">
+                <SelectValue placeholder="Select an account" />
+            </SelectTrigger>
+            <SelectContent>
+                {personals.length > 0
+                    ? <SelectGroup>
+                        <SelectLabel>Personal Account</SelectLabel>
+                        {personals.map(o => <SelectItem key={o.id} value={o.slug}>{o.slug.replace('~$~', '')}</SelectItem>)}
+                    </SelectGroup>
+                    : null}
+                {other.length > 0
+                    ? <SelectGroup>
+                        <SelectLabel>Organisations</SelectLabel>
+                        {other.map(o => <SelectItem key={o.id} value={o.slug}>{o.slug.replace('~$~', '')}</SelectItem>)}
+                        <SelectItem value='~$NEW$~'>
+                            <span className='flex flex-grow justify-between items-center gap-1 align-middle'>
+                                <PlusCircledIcon className='inline' /> Create Organisation
+                            </span>
+                        </SelectItem>
+                    </SelectGroup>
+                    : <SelectGroup>
+                        <SelectLabel>Organisations</SelectLabel>
+                        <SelectItem value='~$NEW$~'>
+                            <span className='flex flex-grow justify-between items-center gap-1 align-middle'>
+                                <PlusCircledIcon className='inline' /> Create Organisation
+                            </span>
+                        </SelectItem>
+                    </SelectGroup>}
+            </SelectContent>
+        </Select>
     );
-});
+};
 
 export default AccountSelector;
