d97c0892dc7892b6ca83e95e5da784966630e53c
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-03-05 15:47:56 +0100

    feat(ui): Revamp `Footer` and `Header` components

--- apps/ui/src/app/partials/Footer.tsx
+++ apps/ui/src/app/partials/Footer.tsx
@@ -10,11 +10,11 @@ const Footer: FC = () => {
     const { data: versions } = api.v0.system.version.useQuery();
 
     return (
-        <footer>
+        <footer className="border-t border-border">
             <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
                 {/* Top area: Blocks */}
-                <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200 dark:border-gray-700">
+                <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 ">
 
                     {/* 1st block */}
                     <div className="sm:col-span-12 lg:col-span-3">
@@ -114,8 +114,10 @@ const Footer: FC = () => {
 
                 </div>
 
+            </div>
+            <div className="border-t border-border">
                 {/* Bottom area */}
-                <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200 dark:border-gray-700">
+                <div className="md:flex md:items-center md:justify-between py-4 md:py-8 max-w-6xl mx-auto px-4 sm:px-6">
 
                     {/* Social links */}
                     <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
