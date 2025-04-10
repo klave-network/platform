300d6a084a04eb2af90d26e7d0545cc36e76e0a2
Florian Guitton <florian@secretarium.com>
2025-04-10 20:27:19 +0100

    chore(ui): Adding serving node information on the footer

--- apps/ui/src/app/partials/Footer.tsx
+++ apps/ui/src/app/partials/Footer.tsx
@@ -120,14 +120,14 @@ const Footer: FC = () => {
                     {/* Social links */}
                     <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
                         <li>
-                            <a href="https://twitter.com/klavenetwork" target='_blank' rel="noreferrer" className="flex justify-center items-center text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300" aria-label="Twitter">
+                            <a href="https://twitter.com/klavenetwork" target='_blank' rel="noreferrer noopener" className="flex justify-center items-center text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300" aria-label="Twitter">
                                 <svg className="w-12 h-12 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
                                 </svg>
                             </a>
                         </li>
                         <li className="ml-4">
-                            <a href="https://github.com/klave-network" target='_blank' rel="noreferrer" className="flex justify-center items-center text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300" aria-label="Github">
+                            <a href="https://github.com/klave-network" target='_blank' rel="noreferrer noopener" className="flex justify-center items-center text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300" aria-label="Github">
                                 <svg className="w-12 h-12 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
                                 </svg>
@@ -140,11 +140,10 @@ const Footer: FC = () => {
                         {/* <Link to="#" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Terms</Link> · <Link to="#" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Privacy Policy</Link> ·  */}
                         <a className="text-klave-dark-blue dark:text-klave-light-blue hover:underline pr-0" href="https://secretarium.com/">Secretarium</a> © {new Date().getFullYear()} All rights reserved<br />
                         Klave <i title={versions?.git?.branch}>v{versions?.version} ({versions?.git?.commit?.substring(0, 8)}{versions?.git?.dirty ? '*' : ''})</i> - UI <i title={import.meta.env.VITE_REPO_BRANCH}>v{import.meta.env.VITE_REPO_VERSION} ({import.meta.env.VITE_REPO_COMMIT?.substring(0, 8)}{import.meta.env.VITE_REPO_DIRTY ? <i title='Dirty'>*</i> : ''})</i> -
-                        Core <i title={versions?.secretarium?.core}>v{versions?.secretarium.core} ({versions?.secretarium.backend?.core_version?.build_number})</i> - WASM <i title={versions?.secretarium?.wasm}>v{versions?.secretarium.wasm} ({versions?.secretarium.backend?.wasm_version?.build_number})</i>
+                        Core <i title={versions?.secretarium?.core}>v{versions?.secretarium.core} ({versions?.secretarium.backend?.core_version?.build_number})</i> - WASM <i title={versions?.secretarium?.wasm}>v{versions?.secretarium.wasm} ({versions?.secretarium.backend?.wasm_version?.build_number})</i><br />
+                        <span className='text-gray-400 dark:text-gray-300'>Node {window.klaveFrontConfig.KLAVE_UI_NODE__}</span>
                     </div>
-
                 </div>
-
             </div>
         </footer>
     );
--- apps/ui/src/klave-endpoints.d.ts
+++ apps/ui/src/klave-endpoints.d.ts
@@ -1,5 +1,6 @@
 declare interface Window {
     klaveFrontConfig: {
+        KLAVE_UI_NODE__: string;
         KLAVE_API__: string;
         KLAVE_AUTH__: string;
         SECRETARIUM_ID__: string;
