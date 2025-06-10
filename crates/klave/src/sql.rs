ca9af475659f5dd856b0f30fabe6f3ca90dbdd67
Nicolas Marie <nicomarie@gmail.com>
2025-06-10 13:53:30 +0100

    feat(sdk): KN-315 - Add Rust PostGre Management (#358)

---
+++ crates/klave/src/sql.rs
@@ -0,0 +1,22 @@
+use crate::sdk;
+
+pub fn connection_open(uri: &str) -> Result<String, Box<dyn std::error::Error>> {
+    match sdk::sql_connection_open(uri) {
+        Ok(opaque_handle) => Ok(opaque_handle),
+        Err(err) => Err(err.into()),
+    }
+}
+
+pub fn query(connection: &str, query: &str) -> Result<String, Box<dyn std::error::Error>> {
+    match sdk::sql_query(connection, query) {
+        Ok(result) => Ok(result),
+        Err(err) => Err(err.into()),
+    }
+}
+
+pub fn execute(connection: &str, query: &str) -> Result<String, Box<dyn std::error::Error>> {
+    match sdk::sql_exec(connection, query) {
+        Ok(result) => Ok(result),
+        Err(err) => Err(err.into()),
+    }
+}
