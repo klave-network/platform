47e533cf7150523d357691eacd3f8e48fa84fc61
Florian Guitton <florian@secretarium.com>
2025-06-12 11:23:16 +0100

    chore(ui): Ignore Zod type mismatch

--- apps/ui/src/app/utils/useZodForm.ts
+++ apps/ui/src/app/utils/useZodForm.ts
@@ -1,6 +1,17 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, UseFormProps } from 'react-hook-form';
 import { z } from 'zod';
+interface Zod3Type<O = unknown, I = unknown> {
+    _output: O;
+    _input: I;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    _zod: any;
+    _def: {
+        typeName?: string;
+    };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    '~standard': any
+}
 
 export const useZodForm = <TSchema extends z.ZodType>(
     props: Omit<UseFormProps<TSchema['_input']>, 'resolver'> & {
@@ -9,7 +20,7 @@ export const useZodForm = <TSchema extends z.ZodType>(
 ) => {
     const form = useForm<TSchema['_input']>({
         ...props,
-        resolver: zodResolver(props.schema, undefined)
+        resolver: zodResolver(props.schema as unknown as Zod3Type, undefined)
     });
 
     return form;
