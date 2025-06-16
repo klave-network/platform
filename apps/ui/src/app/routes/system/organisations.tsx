979504d1600b16df5a19b2adfc7514f3488c8a95
Florian Guitton <florian@secretarium.com>
2025-06-16 16:33:47 +0200

    chore(ui): Augment the number of organisation fetched for display in the admin view

--- apps/ui/src/app/routes/system/organisations.tsx
+++ apps/ui/src/app/routes/system/organisations.tsx
@@ -22,7 +22,7 @@ function Organisations() {
     //react-query has an useInfiniteQuery hook just for this situation!
     const { data, fetchNextPage, isFetching, isLoading } = api.v0.organisations.infiniteOrganisations.useInfiniteQuery({
         filterUnitialized: true,
-        limit: 50
+        limit: 500
     }, {
         getNextPageParam: (lastPage) => lastPage.nextCursor
     });
