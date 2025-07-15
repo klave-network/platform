6d3c728abcf69eeb35131e67b6a901bbcc2985c9
Florian Guitton <florian@secretarium.com>
2025-07-15 17:07:10 +0100

    chore: Update dependencies

--- apps/ui/src/app/utils/useZodForm.ts
+++ apps/ui/src/app/utils/useZodForm.ts
@@ -1,26 +1,22 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm, UseFormProps } from 'react-hook-form';
-import { z } from 'zod';
-interface Zod3Type<O = unknown, I = unknown> {
-    _output: O;
-    _input: I;
+import { useForm, UseFormProps, FieldValues } from 'react-hook-form';
+import { ZodType } from 'zod';
+
+type Zod3Type<O = unknown, I = unknown> = ZodType<O, I> & {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     _zod: any;
-    _def: {
-        typeName?: string;
-    };
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     '~standard': any
 }
 
-export const useZodForm = <TSchema extends z.ZodType>(
-    props: Omit<UseFormProps<TSchema['_input']>, 'resolver'> & {
-        schema: TSchema;
+export const useZodForm = <T extends FieldValues, C = unknown, V = T>(
+    props: UseFormProps<T, C, V> & {
+        schema: Zod3Type<unknown, T>
     }
 ) => {
-    const form = useForm<TSchema['_input']>({
+    const form = useForm<T, C, V>({
         ...props,
-        resolver: zodResolver(props.schema as unknown as Zod3Type, undefined)
+        resolver: zodResolver(props.schema)
     });
 
     return form;
