c66ad386d83fca7d4c07784c365a5916b35dcc00
Florian Guitton <florian@secretarium.com>
2025-07-21 19:48:04 +0100

    fix(ui): Add copy-able commands

--- apps/ui/src/app/routes/welcome.tsx
+++ apps/ui/src/app/routes/welcome.tsx
@@ -1,3 +1,4 @@
+import { MouseEventHandler } from 'react';
 import { Link } from 'react-router-dom';
 import * as Tabs from '@radix-ui/react-tabs';
 import { ExternalLinkIcon } from '@radix-ui/react-icons';
@@ -5,8 +6,21 @@ import Expl_1 from '../images/example_thumb/expl_1.svg';
 import Expl_2 from '../images/example_thumb/expl_2.svg';
 import Expl_3 from '../images/example_thumb/expl_3.svg';
 import Expl_4 from '../images/example_thumb/expl_4.svg';
+import { UilCopy } from '@iconscout/react-unicons';
 
 function Welcome() {
+
+    const copyToClipboard: MouseEventHandler<HTMLSpanElement> = (e) => {
+        e.preventDefault();
+        const text = e.currentTarget.textContent || '';
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                //
+            }).catch(() => {
+                //
+            });
+    };
+
     return <>
         <div className="sm:px-7 sm:pt-7 px-4 py-4 flex flex-col w-full border-b border-gray-200 bg-white dark:bg-gray-900 dark:text-white dark:border-gray-800 sticky top-0">
             <div className="flex w-full items-center">
@@ -39,15 +53,14 @@ function Welcome() {
                         <span className='inline-block h-8 w-8 -ml-1 mt-5 mb-3 border rounded-full font-bold text-center'>1</span>
                         <span className='mt-0 font-bold block'>Check you have Node installed (Node &gt;= v24)</span>
                         Get info on how to install Node at <a rel="noopener" href='https://nodejs.org' target='_blank'>https://nodejs.org</a><br />
-                        <pre className='text-left'>
-                            &gt; node -v<br />
+                        <pre className='text-left overflow-hidden relative text-slate-500'>
+                            &gt;&nbsp;&nbsp;<span className='relative'><span className='text-white before:-mx-2 before:-my-1 before:rounded before:pr-6 before:absolute before:w-[calc(100%+1rem)] before:h-8 hover:before:bg-gray-700 hover:cursor-pointer group' onClick={copyToClipboard}><span className='relative'><UilCopy className='h-4 w-4 mr-2 hidden group-hover:inline-block' />node -v</span></span></span><br />
                             v24.0.2
                         </pre>
                         <span className='inline-block h-8 w-8 -ml-1 mt-3 mb-3 border rounded-full font-bold text-center'>2</span>
                         <span className='mt-0 font-bold block'>Scaffold your Klave application</span>
-                        <pre className='text-left'>
-                            &gt; yarn create on-klave<br />
-                            <br />
+                        <pre className='text-left overflow-hidden relative text-slate-500'>
+                            &gt;&nbsp;&nbsp;<span className='relative'><span className='text-white before:-mx-2 before:-my-1 before:rounded before:pr-6 before:absolute before:w-[calc(100%+1rem)] before:h-8 hover:before:bg-gray-700 hover:cursor-pointer group' onClick={copyToClipboard}><span className='relative'><UilCopy className='h-4 w-4 mr-2 hidden group-hover:inline-block' />yarn create on-klave</span></span></span><br />
                             √ What is the npm package name? ... my-honest-app<br />
                             √ What is the name of your honest application? ... My honest application<br />
                             √ How would you describe the honest application? ... This is a honest application for Klave<br />
@@ -58,38 +71,33 @@ function Welcome() {
                         </pre>
                         <span className='inline-block h-8 w-8 -ml-1 mt-3 mb-3 border rounded-full font-bold text-center'>3</span>
                         <span className='mt-0 font-bold block'>Get your code ready</span>
-                        Check out Klave documentation to know more about the available APIs.<br />
+                        Open your favorite IDE and check out Klave documentation to know more about the available APIs.<br />
                         <a rel="noopener" href='https://klave.com/docs' target='_blank'>https://klave.com/docs</a>
                         <br />
                         <span className='inline-block h-8 w-8 -ml-1 mt-10 mb-3 border rounded-full font-bold text-center'>4</span>
                         <span className='mt-0 font-bold block'>Push your application to Github</span>
-                        <pre className='text-left'>
-                            &gt; gh repo create<br />
-                            &gt; git add .<br />
-                            &gt; git commit -m "your commit message"<br />
-                            &gt; git push --set-upstream origin master<br />
-                        </pre>
+                        Push you newly created application repo via `git` or <a rel='noopener' title='Github CLI' target='_blank' href='https://cli.github.com/'>Github CLI</a><br />
                         <span className='inline-block h-8 w-8 -ml-1 mt-10 mb-3 border rounded-full font-bold text-center'>5</span>
-                        <span className='mt-0 font-bold block'>Click on <Link to="/deploy" className="btn btn-md h-8 text-white dark:text-black dark:bg-gradient-to-r dark:from-klave-cyan dark:to-klave-light-blue bg-black dark:hover:bg-gradient-to-l hover:bg-gray-800 ml-3">
-                            <span>Deploy now</span>
-                        </Link></span>
+                        <span className='mt-0 font-bold block'>Click on <Link to="/deploy" className="btn btn-md h-8 text-white dark:text-black dark:bg-gradient-to-r dark:from-klave-cyan dark:to-klave-light-blue bg-black dark:hover:bg-gradient-to-l hover:bg-gray-800 mx-2">
+                            Deploy now
+                        </Link> in the upper right corner</span>
                     </Tabs.Content>
                     <Tabs.Content value="rust">
                         <span className='inline-block h-8 w-8 -ml-1 mt-5 mb-3 border rounded-full font-bold text-center'>1</span>
                         <span className='mt-0 font-bold block'>Check you have cargo-generate installed</span>
                         Get info on how to install cargo-generate at <a rel="noopener" href='https://docs.rs/cargo-generate' target='_blank'>https://docs.rs/cargo-generate</a><br />
-                        <pre className='text-left'>
-                            &gt; cargo -V<br />
+                        <pre className='text-left overflow-hidden relative text-slate-500'>
+                            &gt;&nbsp;&nbsp;<span className='relative'><span className='text-white before:-mx-2 before:-my-1 before:rounded before:pr-6 before:absolute before:w-[calc(100%+1rem)] before:h-8 hover:before:bg-gray-700 hover:cursor-pointer group' onClick={copyToClipboard}><span className='relative'><UilCopy className='h-4 w-4 mr-2 hidden group-hover:inline-block' />cargo -V</span></span></span><br />
                             cargo 1.87.0 (99624be96 2025-05-06)<br />
-                            &gt; cargo install cargo-generate<br />
+                            &gt;&nbsp;&nbsp;<span className='relative'><span className='text-white before:-mx-2 before:-my-1 before:rounded before:pr-6 before:absolute before:w-[calc(100%+1rem)] before:h-8 hover:before:bg-gray-700 hover:cursor-pointer group' onClick={copyToClipboard}><span className='relative'><UilCopy className='h-4 w-4 mr-2 hidden group-hover:inline-block' />cargo install cargo-generate</span></span></span><br />
                             Updating crates.io index<br />
                             Downloaded cargo-generate v0.23.3<br />
                             Installed package `cargo-generate v0.23.3` (executable `cargo-generate`)
                         </pre>
                         <span className='inline-block h-8 w-8 -ml-1 mt-3 mb-3 border rounded-full font-bold text-center'>2</span>
                         <span className='mt-0 font-bold block'>Scaffold your Klave application</span>
-                        <pre className='text-left'>
-                            &gt; cargo generate --git https://github.com/klave-network/rust-template --name your_app_name<br />
+                        <pre className='text-left overflow-hidden relative text-slate-500'>
+                            &gt;&nbsp;&nbsp;<span className='relative'><span className='text-white before:-mx-2 before:-my-1 before:rounded before:pr-6 before:absolute before:w-[calc(100%+1rem)] before:h-8 hover:before:bg-gray-700 hover:cursor-pointer group' onClick={copyToClipboard}><span className='relative'><UilCopy className='h-4 w-4 mr-2 hidden group-hover:inline-block' />cargo generate --git https://github.com/klave-network/rust-template ledger --name your_app_name</span></span></span><br />
                             🔧 Destination: .\your_app_name ...<br />
                             🔧 project-name: your_app_name ...<br />
                             🔧 Generating template ...<br />
@@ -98,21 +106,16 @@ function Welcome() {
                         </pre>
                         <span className='inline-block h-8 w-8 -ml-1 mt-3 mb-3 border rounded-full font-bold text-center'>3</span>
                         <span className='mt-0 font-bold block'>Get your code ready</span>
-                        Check out Klave documentation to know more about the available APIs.<br />
+                        Open your favorite IDE and check out Klave documentation to know more about the available APIs.<br />
                         <a rel="noopener" href='https://klave.com/docs' target='_blank'>https://klave.com/docs</a>
                         <br />
                         <span className='inline-block h-8 w-8 -ml-1 mt-10 mb-3 border rounded-full font-bold text-center'>4</span>
                         <span className='mt-0 font-bold block'>Push your application to Github</span>
-                        <pre className='text-left'>
-                            &gt; gh repo create<br />
-                            &gt; git add .<br />
-                            &gt; git commit -m "your commit message"<br />
-                            &gt; git push --set-upstream origin master<br />
-                        </pre>
+                        Push you newly created application repo via `git` or <a rel='noopener' title='Github CLI' target='_blank' href='https://cli.github.com/'>Github CLI</a><br />
                         <span className='inline-block h-8 w-8 -ml-1 mt-10 mb-3 border rounded-full font-bold text-center'>5</span>
-                        <span className='mt-0 font-bold block'>Click on <Link to="/deploy" className="btn btn-md h-8 text-white dark:text-black dark:bg-gradient-to-r dark:from-klave-cyan dark:to-klave-light-blue bg-black dark:hover:bg-gradient-to-l hover:bg-gray-800 ml-3">
-                            <span>Deploy now</span>
-                        </Link></span>
+                        <span className='mt-0 font-bold block'>Click on <Link to="/deploy" className="btn btn-md h-8 text-white dark:text-black dark:bg-gradient-to-r dark:from-klave-cyan dark:to-klave-light-blue bg-black dark:hover:bg-gradient-to-l hover:bg-gray-800 mx-2">
+                            Deploy now
+                        </Link> in the upper right corner</span>
                     </Tabs.Content>
                 </Tabs.Root>
             </div>
